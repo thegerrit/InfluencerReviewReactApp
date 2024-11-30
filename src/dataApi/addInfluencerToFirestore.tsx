e77d1514b0d1dd167216afdbda48b400f91785bb
thegerrit <garrettvanbeek@gmail.com>
2024-11-29 20:11:31 -0500

    validates addInfluencer against existing social media handles.

--- src/dataApi/addInfluencerToFirestore.tsx
+++ src/dataApi/addInfluencerToFirestore.tsx
@@ -1,5 +1,8 @@
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDocs, where, query } from "firebase/firestore";
 import { db } from "../utils/FirebaseConfig";
+// import ReadInfluencerData from "../model/ReadInfluencerData";
+import { PLATFORMS } from "../utils/Constants";
+import WriteInfluencerData from "../model/WriteInfluencerData";
 
 async function addInfluencerToFirestore(influencerData: any): Promise<string> {
     try {
@@ -10,6 +13,28 @@ async function addInfluencerToFirestore(influencerData: any): Promise<string> {
       console.error("Error adding document: ", e);
       return "";
     }
-  }
+}
 
-  export default addInfluencerToFirestore;
+async function checkHandlesInUse(influencer: WriteInfluencerData): Promise<string[]> {
+  const handlesInUse: string[] = [];
+  const handleChecks = PLATFORMS.map(async (platform: string) => {
+    if (influencer[platform.toLowerCase() as keyof WriteInfluencerData]) {
+      const queryRef = query(collection(db, 'influencers'), where(platform.toLowerCase(), '==', influencer[platform.toLowerCase() as keyof WriteInfluencerData]));
+      const docs = await getDocs(queryRef);
+      if (docs.size > 0) {
+        handlesInUse.push(platform.toLowerCase() + ":" + influencer[platform.toLowerCase() as keyof WriteInfluencerData]);
+      }
+    }
+  });
+
+  await Promise.all(handleChecks);
+
+  if (handlesInUse.length > 0) {
+    const returnMessage = "The following handles are already in use: " + handlesInUse.join(", ");
+    return ["1", returnMessage];
+  } else {
+    return ["0", "all handles are available."];
+  }
+}
+
+export { addInfluencerToFirestore, checkHandlesInUse };
